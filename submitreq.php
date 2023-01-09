d71a3c928608de5c19f974b426cc1b33933398dc
Adam Piontek <adam@73k.us>
2023-01-09 14:17:35 -0500

    remove unnecessary screensize handling

--- submitreq.php
+++ submitreq.php
@@ -2,7 +2,7 @@
 include('global.inc');
 siteheader('Submit Request');
 $referer = $_SERVER['HTTP_REFERER'];
-if (strpos($referer,'submitreq-run.php?screensize=$screensize') !== false)
+if (strpos($referer,'submitreq-run.php') !== false)
 {
 	navbar("index.php");
 } else {
@@ -20,6 +20,6 @@ foreach ($db->query($sql) as $row) {
 $db = null;
 echo "<br><p>Submitting Song:<br>";
 echo "<p>$artist - $title</p>";
-echo "<form method=get action=submitreq-run.php><input type=hidden name=screensize value=$screensize><input type=hidden name=songid value=$songid>Please enter your name or nickname:<br><input type=text name=singer autocomplete=off autofocus><input type=submit></form>";
+echo "<form method=get action=submitreq-run.php><input type=hidden name=songid value=$songid>Please enter your name or nickname:<br><input type=text name=singer autocomplete=off autofocus><input type=submit></form>";
 echo "<p class=info>If you have a common first name, please also enter your last initial or last name.<br>Doing so will help eliminate confusion and reduce the risk of your turn getting skipped.";
 ?>
